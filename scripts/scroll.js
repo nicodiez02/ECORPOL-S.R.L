84aac391b856237a3b070fe767ba1c7f2f5a2e70
Nico <nicoo.diez02@gmail.com>
2021-12-02 21:14:16 -0300

    .gitignore y breves detalles

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -6,14 +6,14 @@ const imagen = document.getElementById('imgNAV');
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
-        imagen.setAttribute('src', '../img/logo.png');
+        imagen.setAttribute('src', './img/logo.png');
         contenedor.classList.replace('contenedor', 'scroll-contenedor')
         grupo.classList.replace('grupo2', 'scroll-grupo2')
         lista.classList.replace('lista', 'scroll-lista')
         
 
     } else if (window.scrollY == 0) {
-        imagen.setAttribute('src', '../img/logoBlanco.png');
+        imagen.setAttribute('src', './img/logoBlanco.png');
         contenedor.classList.replace('scroll-contenedor', 'contenedor')
         grupo.classList.replace('scroll-grupo2', 'grupo2')
         lista.classList.replace('scroll-lista', 'lista')
