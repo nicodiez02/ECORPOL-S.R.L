98be4e0ab8625e91392bf47555490cfd6bea2548
Nico <nicoo.diez02@gmail.com>
2021-12-14 20:33:15 -0300

    Cambios 14-12

--- scripts/mediaQuery.js
+++ scripts/mediaQuery.js
@@ -2,6 +2,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const contenedor = document.getElementById('contenedor-header');
     const contenedor2 = document.getElementById('contenedor_Query');
+   
     const carousel = document.getElementById('carousel_normal');
     const carousel2 = document.getElementById('carousel_responsive');
 
@@ -15,15 +16,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
             contenedor.setAttribute('style', 'display:none')
             contenedor2.setAttribute('style', 'display:flex')
+            
             carousel.setAttribute('style', 'display:none')
             carousel2.removeAttribute('style')
-  
+            
         } else {
             contenedor2.setAttribute('style', 'display:none')
             contenedor.setAttribute('style', 'display:flex')
+
+            
             carousel.removeAttribute('style')
             carousel2.setAttribute('style', 'display:none')
-        
+            
         }
     }
 
